d0446252ade6440ba8e5ced121c2bf17987d9fe1
lgnq <eaf@melexis.com>
2025-12-15 21:22:53 +0800

    Update mlx90382_reg.h

--- inc/mlx90382_reg.h
+++ inc/mlx90382_reg.h
@@ -11,7 +11,15 @@
 #ifndef MLX90382_REG_H_
 #define MLX90382_REG_H_
 
-#define MLX90382_CONFIG_REG                 0x100
+#define MLX90382AA
+
+#if defined MLX90382AA
+#define MLX90382_DSP_BASE_ADDR              0x000
+#define MLX90382_NVRAM_BASE_ADDR            0x100
+
+#define MLX90382_CONFIG_REG                 (MLX90382_NVRAM_BASE_ADDR + 0x00)
+#define MLX90382_PHASE_OFS                  (MLX90382_NVRAM_BASE_ADDR + 0x20)   //Phase/Angle offset before signal conditioning, resolution 360/216 deg (signed 2th-complement)
+#define MLX90382_PWM_PERIOD                 (MLX90382_NVRAM_BASE_ADDR + 0x2E)
 
 #define MLX90382_SOFT_RESET                 0x004
 #define MLX90382_TEMP                       0x03C
@@ -22,9 +30,46 @@
 #define MLX90382_GC_I                       0x04A   //Gain compensated I component
 #define MLX90382_GC_Q                       0x04E   //Gain compensated Q component
 
-#define MLX90382_PHASE_OFS                  0x120   //Phase/Angle offset before signal conditioning, resolution 360/216 deg (signed 2th-complement)
+#define MLX90382_ANA_VERSION                0x0EE
+#define MLX90382_DIG_VERSION                0x0F0
+#elif defined MLX90382AB
+#define MLX90382_DSP_BASE_ADDR              0x000
+#define MLX90382_NVRAM_BASE_ADDR            0x100
+
+#define MLX90382_CONFIG_REG                 (MLX90382_NVRAM_BASE_ADDR + 0x00)
+#define MLX90382_PHASE_OFS                  (MLX90382_NVRAM_BASE_ADDR + 0x20)   //Phase/Angle offset before signal conditioning, resolution 360/216 deg (signed 2th-complement)
+#define MLX90382_PWM_PERIOD                 (MLX90382_NVRAM_BASE_ADDR + 0x2E)
+
+#define MLX90382_SOFT_RESET                 0x004
+#define MLX90382_TEMP                       0x03C
+#define MLX90382_LIN_PHASE                  0x03E   //Angular value after linearization, resolution 360/216 deg; Writable only with LOCK_PHASE = 1; Before delay compensation
+#define MLX90382_SPEED                      0x040
+#define MLX90382_DRIFTC_PHASE               0x046   //Angular value after delay compensation and zero-point offset correction, resolution 360/0x10000 deg
+#define MLX90382_SC_PHASE                   0x048   //Position value after signal conditioning
+#define MLX90382_GC_I                       0x04A   //Gain compensated I component
+#define MLX90382_GC_Q                       0x04E   //Gain compensated Q component
 
 #define MLX90382_ANA_VERSION                0x0EE
 #define MLX90382_DIG_VERSION                0x0F0
+#elif defined MLX90382BA
+#define MLX90382_DSP_BASE_ADDR              0x000
+#define MLX90382_NVRAM_BASE_ADDR            0x200
+
+#define MLX90382_CONFIG_REG                 (MLX90382_NVRAM_BASE_ADDR + 0x00)
+#define MLX90382_PHASE_OFS                  (MLX90382_NVRAM_BASE_ADDR + 0x20)   //Phase/Angle offset before signal conditioning, resolution 360/216 deg (signed 2th-complement)
+#define MLX90382_PWM_PERIOD                 (MLX90382_NVRAM_BASE_ADDR + 0x2E)
+
+#define MLX90382_SOFT_RESET                 0x004
+#define MLX90382_TEMP                       0x038
+#define MLX90382_LIN_PHASE                  0x03A   //Angular value after linearization, resolution 360/216 deg; Writable only with LOCK_PHASE = 1; Before delay compensation
+#define MLX90382_SPEED                      0x03C
+#define MLX90382_DRIFTC_PHASE               0x042   //Angular value after delay compensation and zero-point offset correction, resolution 360/0x10000 deg
+#define MLX90382_SC_PHASE                   0x044   //Position value after signal conditioning
+#define MLX90382_GC_I                       0x048   //Gain compensated I component
+#define MLX90382_GC_Q                       0x04E   //Gain compensated Q component
+
+#define MLX90382_ANA_VERSION                0x0EE
+#define MLX90382_DIG_VERSION                0x0F0
+#endif
 
 #endif
